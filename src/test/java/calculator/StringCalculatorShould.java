f7f4e282e0d20333cdca7e9ab44967da97ffaa85
d2_49491_raja <krajarsrbhilai@gmail.com>
2021-10-16 18:11:27 +0530

    two numbers return value is sum

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -11,12 +11,6 @@ class StringCalculatorShould {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
-
-    @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
-    }
     
     @Test
 	public final void whenOneNumberIsUsedThenReturnValueIsThatSameNumber() {
